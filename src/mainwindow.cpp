8560c6dd1ecd12a7ee5cc8c93b36987bf76d16da
rafirafi <rafirafi.at@gmail.com>
2018-03-11 22:43:38 +0100

    Add db table

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSqlDriver>
+#include <QSqlQuery>
+#include <QSqlError>
+
 #include <QtDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -27,6 +31,19 @@ void MainWindow::dbOpen()
         qDebug() << Q_FUNC_INFO << "db open failed";
         abort();
     }
+
+    QSqlQuery query(db_);
+    const QString &table = "create table if not exists voca ( word varchar[max], meaning varchar[max]);";
+    bool ok = query.prepare(table);
+    if (!ok) {
+        qDebug() << query.executedQuery();
+        abort();
+    }
+    ok = query.exec();
+    if (!ok) {
+        qDebug() << query.executedQuery();
+        abort();
+    }
 }
 
 void MainWindow::dbClose()
@@ -38,4 +55,5 @@ void MainWindow::dbClose()
         abort();
     }
 }
+
 
